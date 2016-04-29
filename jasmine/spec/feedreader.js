b69f662ce36e8432cb99264b4faedf3c980232e8
Gabriel Mojica <niko.mojica@live.com>
2016-04-30 00:45:33 +1000

    test: Create all required test suits

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,7 +42,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe("The menu", function() {
+    describe('The menu', function() {
 
 
         /* TODO: Write a test that ensures the menu element is
@@ -59,6 +59,7 @@ $(function() {
 
     });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -66,11 +67,14 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
+    describe('New Feed Selection', function() {
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
